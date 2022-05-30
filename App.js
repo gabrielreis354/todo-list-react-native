d81884cc9f5fa0b313196d8abba95c093a000b26
Gabriel Reis <gabrielreiscunha6@gmail.com>
2022-05-30 11:33:25 -0300

    Paper Provider adicionado

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/Navigators/StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <PaperProvider>
+        <StackNavigator />
+      </PaperProvider>
     </NavigationContainer>
   );
 }
